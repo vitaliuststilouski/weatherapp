4981df37cebd88a251fba178144954dc0718481f
Vitali.Ustsilouski <v.ustilovsky@itransition.com>
2025-07-06 22:39:38 +0200

    add cities api

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,8 +1,8 @@
 export const config = {
   port: process.env.PORT || 3001,
-  openWeatherApiKey: process.env.OPENWEATHER_API_KEY || "",
+  weatherbitApiKey: process.env.WEATHERBIT_API_KEY || "",
 };
 
-if (!process.env.OPENWEATHER_API_KEY) {
-  console.warn("Warning: OPENWEATHER_API_KEY is not set in .env file");
+if (!process.env.WEATHERBIT_API_KEY) {
+  console.warn("Warning: WEATHERBIT_API_KEY is not set in .env file");
 }
