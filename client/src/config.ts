96613e338e949828165bb41e7471c0f13a2c2f0c
Vitali.Ustsilouski <v.ustilovsky@itransition.com>
2025-07-07 22:34:20 +0200

    update fils before deploy

--- client/src/config.ts
+++
@@ -1,5 +0,0 @@
-const DEV_API_URL = "http://localhost:3001/api";
-
-const PROD_API_URL = "https://weatherapp-j0yb.onrender.com/api";
-
-export const API_URL = import.meta.env.PROD ? PROD_API_URL : DEV_API_URL;
