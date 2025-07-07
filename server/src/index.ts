7319c99463a5ab1cf0c1e2bf108053f65a016426
Vitali.Ustsilouski <v.ustilovsky@itransition.com>
2025-07-07 19:23:44 +0200

    update indexts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import { config } from "dotenv";
-config();
 import express from "express";
+import cors from "cors";
 import weatherRouter from "./api/weather.routes";
 
-config();
+config({ path: __dirname + "/../.env" });
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", weatherRouter);
@@ -26,6 +33,3 @@ app.use(
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-console.log("API testKey:", process.env.WEATHERBIT_API_KEY);
-console.log("RAPIDAPI_KEY:", process.env.RAPIDAPI_KEY);
